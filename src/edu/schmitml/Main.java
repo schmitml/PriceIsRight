be9e48daa2ce6d24a9674b09467c7f5d466dc752
Marc Schmitt <marc.schmitt.mls@gmail.com>
2016-10-04 14:31:45 -0400

    Two player game complete.

--- src/edu/schmitml/Main.java
+++ src/edu/schmitml/Main.java
@@ -1,5 +1,6 @@
 package edu.schmitml;
 
+import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
@@ -8,11 +9,22 @@ import java.util.concurrent.ThreadLocalRandom;
  * MA381
  */
 public class Main {
+    public static int NUMBER_OF_TRIALS = 10000000;
 
     public static void main(String[] args) {
-	// write your code here
         TwoPlayerGame g1 = new TwoPlayerGame();
-        String g1Result = g1.playGame();
+        ArrayList<Double> winPercentages = g1.runGame(NUMBER_OF_TRIALS);
+
+        int bestStop = 0;
+        double bestPercent = winPercentages.get(0);
+        for(int i = 0; i < 21; i++){
+            if(winPercentages.get(i) >= bestPercent){
+                bestPercent = winPercentages.get(i);
+                bestStop = i;
+            }
+            System.out.println("Stop = " + i + ": " + winPercentages.get(i));
+        }
+        System.out.println("\nBest stop " + bestStop +" with " + bestPercent);
     }
 
 
--- src/edu/schmitml/TwoPlayerGame.java
+++ src/edu/schmitml/TwoPlayerGame.java
@@ -1,27 +1,69 @@
 package edu.schmitml;
 
+import java.util.ArrayList;
+
 /**
- * Class that replicates a two person "Big Wheel" game. Attempts to find the optimal stopping value for player one.
+ * Class that replicates a two person "Big Wheel" game. Attempts to find the optimal stopping value for playerSpins one.
  *
  * Created by Marc Schmitt
  * schmitml@rose-hulman.edu
  * MA381
  */
 public class TwoPlayerGame {
-    private int stop;
 
-    public String playGame() {
-        int spin; //Value of spin
-        for(int i = 0; i < 20; i++){
-            // Pick a stopping value stop
-            stop = i;
+    public ArrayList<Double> runGame(int numberOfTrials){
+        ArrayList<Double> winPercentage = new ArrayList<>();
 
-            spin = Main.spinWheel(); // Spin the wheel
-
-
+        for(int j = 0; j < 21; j++){
+            double wins = 0;
+            for(int i = 0; i < numberOfTrials; i++){ // Run 10000 trials
+                wins += playGame(j); // A trial is for one stopping number
+            }
+            winPercentage.add(j, wins/numberOfTrials);
         }
 
-
-        return "Not yet implemented";
+        return winPercentage;
     }
+
+    /**
+     * Simulates one game.
+     *
+     * @param stop - the stopping value for player one.
+     * @return - +1 if player one won
+     *           0 if player one lost
+     */
+    public int playGame(int stop) {
+        int playerOneScore = playerSpins(stop);
+        if(playerOneScore > 20){
+            return 0;
+        }
+        int playerTwoScore = playerSpins(playerOneScore);
+        if(playerTwoScore > 20){
+            return 1;
+        }
+
+        if(playerOneScore >= playerTwoScore){ // Tie goes to player one
+            return 1;
+        }
+        return 0;
+    }
+
+    /**
+     * For a two person game each playerSpins has a similar strategy. The difference between the two players is where
+     * they get their stopping number from. Player one has to determine for themselves, which we simulate by trying
+     * every option, whereas playerSpins two has their stopping number determined by playerSpins one's score.
+     *
+     * @param stop - the number the playerSpins will stop at
+     * @return - the playerSpins's score
+     */
+    private int playerSpins(int stop){
+        int spin = Main.spinWheel(); // Spin the wheel
+
+        if (spin <= stop){
+            spin += Main.spinWheel();
+        }
+
+        return spin;
+    }
+
 }
